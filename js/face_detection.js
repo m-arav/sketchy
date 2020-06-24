d3f0e8fd30d40ccb0fb32cd412c132e6b1f2ef2e
Aravind <thearavindmurali@gmail.com>
2020-06-25 02:54:17 +0530

    Add face detection with face-api - tensorflowjs

---
+++ js/face_detection.js
@@ -0,0 +1,34 @@
+
+const video = document.querySelector('#video');
+const displaySize = {width: video.width, height: video.height}
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => video.srcObject = stream)
+    .catch(err => console.error(err));
+}
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('vendor/face-api/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('vendor/face-api/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('vendor/face-api/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('vendor/face-api/models')
+]).then(startVideo)
+
+
+video.addEventListener('playing', ()=> {
+    const canvas = faceapi.createCanvasFromMedia(video);    
+    document.body.append(canvas);
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(detectFace, 100, canvas);
+});
+
+async function detectFace(canvas) {
+    const detections = await faceapi.detectAllFaces(video,
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();    
+    const resizedResults = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedResults);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedResults);
+    faceapi.draw.drawFaceExpressions(canvas, resizedResults);
+}
